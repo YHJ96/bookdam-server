d17c6cba2a94869258dc1ccec435479fd3221531
YHJ96 <yhj878812@naver.com>
2024-12-03 03:27:37 +0900

    feat: auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { BookmarkModule } from '../bookmark/bookmark.module';
 import { TrashModule } from '../trash/trash.module';
 import { TagModule } from '../tag/tag.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [BookmarkModule, TrashModule, TagModule, PrismaModule],
+  imports: [BookmarkModule, TrashModule, TagModule, AuthModule, PrismaModule],
   controllers: [AppController],
 })
 export class AppModule {}
