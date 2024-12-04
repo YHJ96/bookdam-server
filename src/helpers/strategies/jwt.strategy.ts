365b91168f28a691720d87d3cbfa7dfff3c04e23
YHJ96 <yhj878812@naver.com>
2024-12-04 20:34:04 +0900

    feat: access token exp check

--- src/helpers/strategies/jwt.strategy.ts
+++ src/helpers/strategies/jwt.strategy.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(configService: ConfigService) {
+  constructor(configService: ConfigService, jwtService: JwtService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request) => {
-          const refreshCookie = request.cookies?.['refresh'];
           const accessCookie = request.cookies?.['access'];
+          const refreshCookie = request.cookies?.['refresh'];
+          const { exp } = jwtService.decode(accessCookie) as { exp: number };
+          const currentTime = Math.floor(Date.now() / 1000);
 
-          return accessCookie || refreshCookie || null;
+          if (exp > currentTime) return accessCookie;
+
+          return refreshCookie || null;
         },
       ]),
       ignoreExpiration: false,
